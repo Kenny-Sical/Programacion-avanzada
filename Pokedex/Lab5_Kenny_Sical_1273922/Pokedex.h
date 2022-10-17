44e76880660d8eca7498557352843dcab7f90063
Kenny-Sical <sicalkenny21@gmail.com>
2022-10-16 18:57:21 -0600

    Lectura de archivo de texto

--- Pokedex/Lab5_Kenny_Sical_1273922/Pokedex.h
+++ Pokedex/Lab5_Kenny_Sical_1273922/Pokedex.h
@@ -8,6 +8,7 @@ namespace Lab5KennySical1273922 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace System::IO;
 
 	/// <summary>
 	/// Resumen de Pokedex
@@ -34,6 +35,9 @@ namespace Lab5KennySical1273922 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::OpenFileDialog^ LectorTXT;
+	protected:
+	private: System::Windows::Forms::Button^ button1;
 
 	private:
 		/// <summary>
@@ -48,12 +52,56 @@ namespace Lab5KennySical1273922 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Pokedex";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Pokedex::typeid));
+			this->LectorTXT = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// LectorTXT
+			// 
+			this->LectorTXT->FileName = L"LectorTXT";
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::OrangeRed;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Unispace", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::SystemColors::ControlLightLight;
+			this->button1->Location = System::Drawing::Point(510, 520);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(125, 54);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Actualizar datos";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &Pokedex::button1_Click);
+			// 
+			// Pokedex
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(845, 619);
+			this->Controls->Add(this->button1);
+			this->Name = L"Pokedex";
+			this->Text = L"Pokedex";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		
+		if (System::Windows::Forms::DialogResult::OK == LectorTXT->ShowDialog()) {
+			StreamReader ^ InputStream = gcnew StreamReader(LectorTXT->FileName);
+			if ( InputStream != nullptr)
+			{
+				while (String^ lineOfText = InputStream->ReadLine()) 
+				{
+
+				}
+			}
+		}
+
+	}
 	};
 }
